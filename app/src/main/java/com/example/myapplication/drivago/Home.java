941f1bc7a9c4529e1e86c9cd45246d77a3f9226a
NikitaF99 <nikkiqwerty979@gmail.com>
2020-10-02 00:33:07 +0530

    getting user count from database added

--- app/src/main/java/com/example/myapplication/drivago/Home.java
+++ app/src/main/java/com/example/myapplication/drivago/Home.java
@@ -5,10 +5,17 @@
  import android.view.View;
  import android.widget.Button;
  import android.widget.ImageButton;
+ import android.widget.TextView;
 
+ import androidx.annotation.NonNull;
  import androidx.appcompat.app.AppCompatActivity;
 
  import com.google.firebase.auth.FirebaseAuth;
+ import com.google.firebase.database.DataSnapshot;
+ import com.google.firebase.database.DatabaseError;
+ import com.google.firebase.database.DatabaseReference;
+ import com.google.firebase.database.FirebaseDatabase;
+ import com.google.firebase.database.ValueEventListener;
 
  public class Home extends AppCompatActivity {
      private ImageButton button1;
@@ -16,11 +23,31 @@
      private ImageButton vehi_btn;
      Button btnSignOut;
      FirebaseAuth mAuth;
+     TextView user;
 
      @Override
      protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
          setContentView(R.layout.activity_home);
+
+         DatabaseReference mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+         //signs is used temporarily
+         mDatabaseRef.child("Signs").addListenerForSingleValueEvent(new ValueEventListener() {
+             @Override
+             public void onDataChange(DataSnapshot dataSnapshot) {
+                 long count= dataSnapshot.getChildrenCount();
+                 user = findViewById(R.id.users);
+
+                 String str = Long.toString(count);
+                 user.setText(str);
+             }
+
+             @Override
+             public void onCancelled(@NonNull DatabaseError error) {
+
+             }
+         });
+
 
          mAuth = FirebaseAuth.getInstance();
 
