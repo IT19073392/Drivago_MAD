186a36936bf641c8156d37f442787bcdb3b675f6
NikitaF99 <nikkiqwerty979@gmail.com>
2020-10-03 00:13:33 +0530

    UI changes added

--- app/src/main/java/com/example/myapplication/drivago/MainActivity.java
+++ app/src/main/java/com/example/myapplication/drivago/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
@@ -21,7 +22,7 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     EditText mEmail, Password;
-    ImageButton mLogin;
+    Button mLogin;
 
     ProgressBar progressBar;
     FirebaseAuth fAuth;
